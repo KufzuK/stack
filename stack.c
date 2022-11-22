9a9bfeaff6733d71c798303593b81cc5b764afe4
KufzuK <vologin.vadim.v@gmail.com>
2022-11-22 21:12:38 +0300

    exits if some mistakes detected, some comments as prepoeation to the next stage, some warnings left

--- stack.c
+++ stack.c
@@ -11,15 +11,22 @@ struct Stack
 	int capacity;
 };
 
-Stack *stack_ctor (void)
+Stack *stack_ctor (/*error_t* error*/)
 {
+	/*if (error == NULL)
+		return NULL;*/
+	
 	Stack *stack = (Stack *)calloc (1, sizeof (Stack));	
 	if (stack == NULL)
+	{
+		//*error = null_ptr_arg;
 		return NULL;
+	}
 
 	stack->data = (elem_t *)calloc (init_capacity, sizeof (elem_t));
 	if (stack->data == NULL)
 	{
+		//*error = null_ptr;
 		free (stack);
 		return NULL;
 	}
@@ -33,7 +40,6 @@ void stack_dtor (Stack *stack)
 {
 	if (stack == NULL)
 		return;
-
 	free (stack->data);
 	free (stack);
 }
@@ -48,11 +54,9 @@ void stack_push(Stack *stack, const elem_t value)
 	if (stack->data == NULL)
 		return;
 
-	change_size(stack);
-
 	if (stack->size == stack->capacity)
 	{
-		double_capacity(stack);
+		double_capacity(stack); // too big capacity
 	}
 
 	stack->data[stack->size++] = value;
@@ -62,10 +66,12 @@ void stack_pop(Stack *stack)
 {
 	if (stack == NULL)
 	   	return;	
+	if (stack->data == NULL)
+		return;
 
 	if (stack->size < stack->capacity / 4)
 	{
-		reduce_capacity(stack);
+		reduce_capacity(stack); // zero capacity
 	}
 
 	stack->data[stack->size--];
@@ -75,20 +81,42 @@ void stack_pop(Stack *stack)
 
 elem_t stack_top(Stack *stack)
 {
-	/*if (stack == NULL)
-		return NULL;*/
+	if (stack == NULL)
+		return 1; 
+
+	if (stack->data == NULL)
+		return 2;
 
 	return stack->data[stack->size];
 }
 
 void double_capacity(Stack *stack)
 {
+	if (stack == NULL)
+		return;
+
+	/*if (stack->capacity >= MAX_MEMORY_ALLOWED / 2)
+		return;*/
+	
 	realloc(stack, 2 * stack->capacity * sizeof(elem_t));
 	stack->capacity *= 2;
+
+	if (stack == NULL)
+		return;
 }
 
 void reduce_capacity(Stack *stack)
 {
+	if (stack == NULL)
+		return;
+	
 	realloc(stack, stack->capacity / 2 * sizeof(elem_t));
 	stack->capacity /= 2;
+
+	if (stack == NULL)
+		return;
+
+	if (stack->capacity <= 0)
+		return;
+	
 }
